5eda4dc83f7acff59d054a9d5b6e40c6f2bc7ec4
Chen <286298650@qq.com>
2019-09-03 17:30:35 +0800

    添加http请求说明，jmeter测试脚本

--- http.go
+++ http.go
@@ -3,26 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
-//GET请求实例
+//获取get参数
 func hello(w http.ResponseWriter, request *http.Request) {
 	query := request.URL.Query() //实例化对象
 	name := query.Get("name")    //获取get参数
-	fmt.Fprintf(w, name)         //输出
+	fmt.Println(time.Now(),"成功获取GET参数:",name)
+	fmt.Fprintf(w,name)         //输出
 }
 
-//post请求实例
+//获取POST参数
 func word(w http.ResponseWriter, request *http.Request) {
 	name := request.PostFormValue("name")//获取post参数
 	fmt.Fprintf(w, name)
+	fmt.Println(time.Now(),"成功获取POST参数:", name)
 }
 
 //主函数
 func main() {
+	fmt.Println("后台等待中...")
 	//设置端口和ip地址
 	server := &http.Server{
-		Addr: "0.0.0.0:80",
+		Addr: ":80",
+		Handler:nil,
 	}
 	http.HandleFunc("/hello", hello) //路由转发到hello函数
 	http.HandleFunc("/word", word)   //路由转发至word函数
